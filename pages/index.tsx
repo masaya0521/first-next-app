ec613dd9829382dc212669ebafdefc8dfccacee4
masaya0521 <kaji0521km@gmail.com>
2022-08-08 22:40:00 +0900

    ページ遷移の追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,17 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Sample from "./sample";
+import Link from "next/link";
 
 const Home: NextPage = () => {
-  return <div className={styles.container}>test</div>;
+  return (
+    <div>
+      <div className={styles.container}>test</div>
+      <Link href="ssr">
+        <a>go to ssr</a>
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
